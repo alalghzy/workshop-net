3ade82526cc7d9b541e333347815fe9881a1f424
alalghzy <alalghzy@gmail.com>
2023-10-08 12:47:53 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body"w>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                    <a href="{{ url('/posts') }}"><button class="btn btn-primary">CRUD</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
